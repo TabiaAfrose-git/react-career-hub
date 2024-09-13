c26c211659a231cc47c18ddb2fa7f0f0425be3be
TabiaAfrose-git <tabiaafrose16@gmail.com>
2024-09-13 09:17:49 +0600

    add too many componets and loading data from jabs.json

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,14 @@
+import Banner from "../Banner/Banner";
+import CatagoryList from "../CatagoryList/CatagoryList";
+import Featuredjobs from "../Featuredjobs/Featuredjobs";
 
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <CatagoryList></CatagoryList>
+            <Featuredjobs></Featuredjobs>
             <h2>This is Home!</h2>
         </div>
     );
